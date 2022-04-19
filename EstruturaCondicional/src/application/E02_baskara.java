25e1d6f3934ea4cad0470d1a44be377d907d67d3
Ana Carolina Bilecki <anacarolina255@gmail.com>
2022-04-19 14:19:51 -0300

    Solve question 2

---
+++ EstruturaCondicional/src/application/E02_baskara.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class E02_baskara {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double a, b, c, delta, x1, x2;
+		
+		System.out.print("Coeficiente a: ");
+		a = sc.nextDouble();
+		System.out.print("Coeficiente b: ");
+		b = sc.nextDouble();
+		System.out.print("Coeficiente c: ");
+		c = sc.nextDouble();
+		
+		delta = Math.pow(b, 2) - 4.0 * a * c;
+		
+		if (a == 0 || delta < 0) {
+			System.out.println("Esta equação não possui raízes reais");
+		} else {
+			x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+			x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+			System.out.printf("X1 = %.4f%n", x1);
+			System.out.printf("X2 = %.4f", x2);
+		}
+		
+		sc.close();
+	}
+}
