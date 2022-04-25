9241fe1afa3ffbe83ce0684c5d608d0224122bf3
Ana Carolina Bilecki <anacarolina255@gmail.com>
2022-04-25 14:10:09 -0300

    Solve question 5

---
+++ EstruturasRepetitivas/src/application/E05_validacao_de_nota.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class E05_validacao_de_nota {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double nota1, nota2, media;
+		
+		System.out.print("Digite a primeira nota: ");
+		nota1 = sc.nextDouble();
+		
+		while (nota1 < 0 || nota1 > 10) {
+			System.out.print("Valor inválido! Tente novamente: ");
+			nota1 = sc.nextDouble();
+		}
+		
+		System.out.print("Digite a segunda nota: ");
+		nota2 = sc.nextDouble();
+		
+		while (nota2 < 0 || nota2 > 10) {
+			System.out.print("Valor inválido! Tente novamente: ");
+			nota2 = sc.nextDouble();
+		}
+		
+		media = (nota1 + nota2) / 2.0;
+		
+		System.out.printf("MÉDIA = %.2f", media);
+		
+		sc.close();
+	}
+}
