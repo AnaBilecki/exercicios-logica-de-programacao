e482dcc48b8d3754904223572e677cd0c078cc1a
Ana Carolina Bilecki <anacarolina255@gmail.com>
2022-04-25 13:56:10 -0300

    Solve question 3

---
+++ EstruturasRepetitivas/src/application/E03_senha_fixa.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+public class E03_senha_fixa {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int senha;
+		
+		System.out.print("Digite a senha: ");
+		senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.print("Senha Inválida! Tente novamente: ");
+			senha = sc.nextInt();
+		}
+		
+		System.out.print("Acesso permitido!");
+		
+		sc.close();
+	}
+}
